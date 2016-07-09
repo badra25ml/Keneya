eaffafa8087e94de616c8df2a99cb7dd23456221
Aliou Maiga <alioumaiga@alious-mbp.attlocal.net>
2016-07-09 13:07:21 -0700

    Fixed language selection

--- keneya-play/app/controllers/HomeController.scala
+++ keneya-play/app/controllers/HomeController.scala
@@ -6,8 +6,6 @@ import play.api._
 import play.api.mvc._
 import play.api.i18n.{I18nSupport, MessagesApi, Messages, Lang}
 
-
-
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
@@ -15,12 +13,30 @@ import play.api.i18n.{I18nSupport, MessagesApi, Messages, Lang}
 @Singleton
 class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport{
 
+  
+  
   /**
    * Create an Action to render an HTML page with a welcome message.
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
+//  def index = Action { request =>
+//  Ok("Languages: " + request.acceptLanguages.map(_.code).mkString(", "))
+//}
+//  
+  def changeLangfrench = Action{ implicit request =>
+    val locale = request.getQueryString("locale").getOrElse("fr")
+    Ok(views.html.index("Your new application is ready.")).withLang(Lang(locale))   
+  }
+  
+  def changeLangbambara = Action{ implicit request =>
+    val bambara = request.getQueryString("locale").getOrElse("bm")
+    Ok(views.html.index("Your new application is ready.")).withLang(Lang(bambara))
+    
+    
+  }
+  
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
@@ -38,5 +54,6 @@ class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller 
       def bambara = Action {
     Ok(views.html.bambara("keneya Bambara page"))
   }
+     
 }
 
